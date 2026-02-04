ac30e827aec5f2accafc4bd7e880f7f025a84420
sahil00018 <sahilsahoo2222@gmail.com>
2026-02-04 19:39:11 +0530

    Day 2: Custom User model with roles

--- accounts/models.py
+++ accounts/models.py
@@ -1,3 +1,15 @@
+from django.contrib.auth.models import AbstractUser
 from django.db import models
 
-# Create your models here.
+class User(AbstractUser):
+
+    ROLE_CHOICES = (
+        ('CANDIDATE', 'Candidate'),
+        ('RECRUITER', 'Recruiter'),
+        ('ADMIN', 'Admin'),
+    )
+
+    role = models.CharField(max_length=20, choices=ROLE_CHOICES, default='CANDIDATE')
+
+    def __str__(self):
+        return f"{self.username} - {self.role}"
